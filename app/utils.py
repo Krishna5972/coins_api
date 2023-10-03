a1b8afd0a48220571a8f5b3eb60d65664323294d
vamsi <vamsikrishnagannamaneni@gmail.com>
2023-10-03 11:16:06 -0500

    Add 5m endpoint

--- app/utils.py
+++ app/utils.py
@@ -21,6 +21,10 @@ def custom_memoize(f):
             timeout = 59 * 60  # 1 hour
             if current_minute == 0:
                 cache.delete(f"{f.__name__}-1h")
+        elif args[0] == '5m':
+            timeout = 4.9 * 60  # 5m
+            if current_minute == 0:
+                cache.delete(f"{f.__name__}-5m")
         else:
             timeout = None  # Default
 
@@ -48,6 +52,54 @@ def custom_memoize_get_coins(f):
         timeout = 840
 
         cache_key = f"{f.__name__}"
+
+        result = cache.get(cache_key)
+        if result is not None:
+            return result
+
+        # If not in cache, compute and then cache the result
+        result = f(*args, **kwargs)
+        cache.set(cache_key, result, timeout=timeout)
+        return result
+
+    return decorated_function
+
+
+def custom_memoize_first_volatile(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+
+        current_time = datetime.utcnow()
+       
+        current_minute = current_time.minute
+        current_hour = current_time.hour
+        current_second  = current_time.second
+
+
+        cache_key = f"{f.__name__}-{args[0]}"  #custom_memoize_first_volatile-1
+
+        if current_hour == 0 and current_minute == 0 and current_second < 30:
+            cache.delete(f"{f.__name__}-{args[0]}")
+
+        result = cache.get(cache_key)
+        if result is not None:
+            return result
+
+        # If not in cache, compute and then cache the result
+        result = f(*args, **kwargs)
+        cache.set(cache_key, result)
+        return result
+    return decorated_function
+
+
+
+def memoize_get_screener_data(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Choose the cache timeout based on the first argument
+        timeout = 180
+
+        cache_key = f"{f.__name__}"  #memoize_get_screener_data
 
         result = cache.get(cache_key)
         if result is not None:
--- run.py
+++ run.py
@@ -2,61 +2,109 @@ from app import create_app,cache
 import logging
 import os
 from app.services.functions import notifier_with_photo,notifier
-from app.services.trading import execute_and_get_results
+from app.services.trading import execute_and_get_results,get_first_volatile_coin
 import threading
 from datetime import datetime,timedelta
 logging.disable(logging.CRITICAL + 1)
 import time
+import warnings
+warnings.filterwarnings("ignore")
+
 
 
 import asyncio
+
 
 async def async_execute_and_get_results(timeframe):
     execute_and_get_results(timeframe)
     await asyncio.sleep(0.1)
 
+
+async def asyn_get_first_volatile_coin(perc):
+    get_first_volatile_coin(perc)
+    await asyncio.sleep(0.1)
+
 async def background_task():
+    print(f'Background task running')
     cache_key_15m = f"execute_and_get_results-15m"
     cache_key_30m = f"execute_and_get_results-30m"
     cache_key_1h = f"execute_and_get_results-1h"
+    cache_key_5m = f"execute_and_get_results-5m"
+
     get_coins_list_cache_key = 'get_coins_list'
+    memoize_get_screener_data_cache_key = 'memoize_get_screener_data'
+
+    current_time = datetime.utcnow()
+    current_minute = current_time.minute
+    current_hour = current_time.hour
+    current_second  = current_time.second
 
     while True:      
         current_time = datetime.utcnow()
         current_minute = current_time.minute
+        current_hour = current_time.hour
+        current_second  = current_time.second
 
         sleep_time = 60
 
+        
+
         if current_minute == 0 :
             cache.delete(get_coins_list_cache_key)
+            cache.delete(cache_key_5m)
             cache.delete(cache_key_15m)
             cache.delete(cache_key_30m)
             cache.delete(cache_key_1h)
             notifier('All cache cleared')
             await asyncio.gather(
+                async_execute_and_get_results('5m'),
                 async_execute_and_get_results('15m'),
                 async_execute_and_get_results('30m'),
                 async_execute_and_get_results('1h')
             )
+            notifier('Calculation is Done for all timeframes')
             time.sleep(sleep_time)
 
-            
+
         elif current_minute % 30 == 0:
             cache.delete(get_coins_list_cache_key)
+            cache.delete(cache_key_5m)
             cache.delete(cache_key_15m)
             cache.delete(cache_key_30m)
             notifier('15m and 30m cache cleared')
             await asyncio.gather(
+                async_execute_and_get_results('5m'),
                 async_execute_and_get_results('15m'),
                 async_execute_and_get_results('30m'),
             )
+            notifier('Calculation is Done for 15m and 30m')
             time.sleep(sleep_time)
         elif current_minute % 15 == 0:
             cache.delete(get_coins_list_cache_key)
             cache.delete(cache_key_15m)
-            execute_and_get_results('15m')
-            notifier('15m cache cleared')
+            cache.delete(cache_key_5m)
+            await asyncio.gather(
+                async_execute_and_get_results('5m'),
+                async_execute_and_get_results('15m'),
+            )
+            notifier('Calculation is Done for 15m')
             time.sleep(sleep_time)
+
+        elif current_minute % 5 == 0:
+            cache.delete(get_coins_list_cache_key)
+            cache.delete(cache_key_5m)
+            execute_and_get_results('5m')
+            time.sleep(sleep_time)
+
+
+        if current_hour == 0 and current_minute == 0 and current_second < 30:
+            cache.delete(memoize_get_screener_data_cache_key)
+            await asyncio.gather(
+                asyn_get_first_volatile_coin(1),
+                asyn_get_first_volatile_coin(1.5)
+            )
+
+        
 
         time.sleep(1)
         
